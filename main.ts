7b4a4fb63284ccaa9c8f60c13d3246f86f17c66e
YurasKamaz <69901570+YurasKamaz@users.noreply.github.com>
2020-10-26 18:01:53 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 // Init BLE services
 bluetooth.startIOPinService()
 bluetooth.startAccelerometerService()
+
 input.onGesture(Gesture.FreeFall, function () {
     bluetooth.uartWriteString("fall;")
 })
 
-function drawLEDs() {
+basic.forever(function () {
+	drawLEDs()
+})
+function drawLEDs () {
     let pins_data = [
         pins.analogReadPin(AnalogPin.P0), 
         pins.analogReadPin(AnalogPin.P1), 
         pins.analogReadPin(AnalogPin.P2), 
         pins.analogReadPin(AnalogPin.P3), 
         pins.analogReadPin(AnalogPin.P4)
-    ]
+        ]
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
+
+
